9533270fe6147bf9b0a5396c49df0dea39536543
Guru Prasad <guruprasadpr@yahoo.com>
2019-08-28 10:18:28 +0530

    Included substraction

--- src/App.js
+++ src/App.js
@@ -7,6 +7,57 @@ function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const addInp = function(){
+  return {
+    value1 : getRandom(0,10),
+    value2 : getRandom(0,10)
+  }
+}
+
+  const mulInp = function(){
+    return {
+      value1 : getRandom(1,3),
+      value2 : getRandom(2,10)
+    }
+  }
+
+    const subInp = function(){
+      var v1 = getRandom(1,10)
+      var v2 = getRandom(0,v1)
+      return {
+        value1 : v1,
+        value2 : v2
+      }
+}
+
+const getInitial = function(func){
+  var obj = func()
+  obj['result'] = ''
+  obj['message'] = ''
+  return obj
+}
+
+
+const add = function(n1,n2){
+  return n1+n2
+}
+
+const sub = function(n1,n2){
+  return n1-n2
+}
+
+const mul = function (n1,n2) {
+  return n1*n2
+}
+
+const calc = function(n1,n2, func){
+  return func(n1,n2)
+}
+
+const isCorrect = function(n1,n2,result,func){
+  return result === calc(n1,n2,func)
+}
+
 function Box1(props){
   return (
     <input  className="box" value={props.val}  readOnly  />
@@ -24,34 +75,17 @@ function Box2(props){
 
 
 class Addition extends Component{
-  state = {
-    value1 : getRandom(0,10),
-    value2 : getRandom(0,10),
-    result : '',
-    message: ''
-  }
+  state = getInitial(addInp)
+
   validateResult =(event) => {
     event.preventDefault()
-    if (this.state.value1 + this.state.value2 === parseInt(this.state.result)){
-      console.log("Correct")
-      this.setState({message: 'Correct'})
-    }else{
-      console.log("Wrong");
-      this.setState({message: 'Wrong'})
-    }
-
+    var msg = isCorrect(this.state.value1,this.state.value2, parseInt(this.state.result), add)?'Correct':'Wrong'
+    this.setState({message: msg})
   }
 
   refreshPage =(event) =>{
-    //window.location.reload();
-    this.setState({
-      value1 : getRandom(0,10),
-      value2 : getRandom(0,10),
-      result : '',
-      message: ''
-    })
+    this.setState(getInitial(addInp))
   }
-
 
   render(){
  return (
@@ -82,32 +116,16 @@ class Addition extends Component{
 }
 
 class Multiplication extends Component{
-  state = {
-    value1 : getRandom(1,3),
-    value2 : getRandom(2,10),
-    result : '',
-    message: ''
-  }
+  state = getInitial(mulInp)
+
   validateResult =(event) => {
     event.preventDefault()
-    if (this.state.value1 * this.state.value2 === parseInt(this.state.result)){
-      console.log("Correct")
-      this.setState({message: 'Correct'})
-    }else{
-      console.log("Wrong");
-      this.setState({message: 'Wrong'})
-    }
-
+    var msg = isCorrect(this.state.value1,this.state.value2, parseInt(this.state.result), mul)?'Correct':'Wrong'
+    this.setState({message: msg})
   }
 
   refreshPage =(event) =>{
-    //window.location.reload();
-    this.setState({
-      value1 : getRandom(1,3),
-      value2 : getRandom(2,10),
-      result : '',
-      message: ''
-    })
+    this.setState(getInitial(mulInp))
   }
 
 
@@ -137,27 +155,71 @@ class Multiplication extends Component{
      </div>
       )
     }
-    }
+}
+
+class Substraction extends Component{
+      state = getInitial(subInp)
+      validateResult =(event) => {
+        event.preventDefault()
+        var msg = isCorrect(this.state.value1,this.state.value2, parseInt(this.state.result), sub)?'Correct':'Wrong'
+        this.setState({message: msg})
+      }
+
+      refreshPage =(event) =>{
+        this.setState(getInitial(subInp))
+      }
+
+
+     render(){
+       return (
+         <div className="App">
+           <header className="App-header">
+      <form  onSubmit={this.validateResult}>
+          <div className="">
+             <Box1 val={this.state.value1}/>
+             <Box1 val="-"/>
+             <Box1 val={this.state.value2}/>
+             <Box1 val="=" />
+             <input className="resbox"
+                    value={this.state.result}
+                    onChange={event => this.setState({result: event.target.value})}
+                    required
+              />
+              <br/>
+              <Box2  val={this.state.message} />
+              <br/><br/>
+              <input  type="submit" value="Submit" />
+         </div>
+        </form>
+            &nbsp;<button type="button" onClick={this.refreshPage}>Next</button>
+            </header>
+         </div>
+       )
+     }
+}
 
 
 export default class App extends React.Component {
-  //this.state = {value: ''};
-
   state = {
    showAddition: false,
-   showMultiplication: false
+   showMultiplication: false,
+   showSubstraction: false
 }
 
   handleAddClick= (props) => {
-    this.setState({showAddition: true, showMultiplication:false})
+    this.setState({showAddition: true, showMultiplication:false, showSubstraction: false})
   }
 
   handleMulClick= (props) => {
-    this.setState({showMultiplication: true, showAddition:false})
+    this.setState({showMultiplication: true, showAddition:false,showSubstraction: false})
+  }
+
+  handleSubClick= (props) => {
+    this.setState({showSubstraction: true, showMultiplication: false, showAddition:false})
   }
 
   render(){
-    const { showAddition,showMultiplication } = this.state
+    const { showAddition,showMultiplication, showSubstraction } = this.state
  return (
  <div className="App">
    <header className="row App-header">
@@ -166,19 +228,24 @@ export default class App extends React.Component {
     </p>
     <div>
     <div  className="row ">
-    <div className="col-xs-2"></div>
-    <div className="col-xs-2 ">
-    <button className="btn btn-lg offset-1" onClick={this.handleAddClick}> + </button>
-    </div>
-    <div className="col-xs-2"></div>
-    <div className="col-xs-2">
-    <button className="btn btn-lg  offset-1" onClick={this.handleMulClick}> X </button>
+      <div className="col-xs-1"></div>
+      <div className="col-xs-2 ">
+      <button className="btn btn-lg offset-1" onClick={this.handleAddClick}> + </button>
+      </div>
+      <div className="col-xs-1"></div>
+      <div className="col-xs-2">
+        <button className="btn btn-lg  offset-1" onClick={this.handleMulClick}> X </button>
+      </div>
+      <div className="col-xs-1"></div>
+      <div className="col-xs-2">
+        <button className="btn btn-lg  offset-1" onClick={this.handleSubClick}> - </button>
+      </div>
     </div>
     </div>
 
     {showAddition && <Addition url='/Addition'/>}
     {showMultiplication && <Multiplication url='/Multiplication'/>}
-    </div>
+    {showSubstraction && <Substraction url='/Substraction'/>}
    </header>
 
  </div>
