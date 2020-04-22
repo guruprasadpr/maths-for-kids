b988a742d2096223d3519dd0d816c615869a9430
Guru Prasad <guruprasadpr@yahoo.com>
2020-04-22 10:25:07 +0530

    Updated mulinp range values

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const addInp = function(){
 
   const mulInp = function(){
     return {
-      value1 : getRandom(2,6),
+      value1 : getRandom(2,10),
       value2 : getRandom(3,10)
     }
   }
